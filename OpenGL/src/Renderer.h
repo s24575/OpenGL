5451bda9e1aab1a28ec88a0d248149705f16fd9f
s24575 <sawickigrzegorz66@gmail.com>
2023-02-13 13:26:07 +0100

    add Renderer class

--- OpenGL/src/Renderer.h
+++ OpenGL/src/Renderer.h
@@ -2,6 +2,10 @@
 
 #include <GL/glew.h>
 
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
 #define ASSERT(x) if(!(x)) __debugbreak();
 
 #ifdef _DEBUG
@@ -12,3 +16,10 @@
 
 void GLClearError();
 bool GLLogCall(const char* function, const char* file, int line);
+
+class Renderer
+{
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& vb, const Shader& shader) const;
+};
